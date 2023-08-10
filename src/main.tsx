0a1d0f1bf5fd7ecba92fef291e7957133509680f
Mondara <mondara.t@hotmail.com>
2023-08-10 14:33:41 +1000

    Commented Login Context.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
-import { ThemeContextProvider, MarkdownProvider, AuthProvider } from './context'
+import { ThemeContextProvider, MarkdownProvider/*, AuthProvider*/ } from './context'
 import App from './App'
 import './index.css'
 
@@ -12,10 +12,10 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeContextProvider>
       <MarkdownProvider>
-        <AuthProvider>
-          <ToastContainer theme="dark" />
-          <App />
-        </AuthProvider>
+        {/* <AuthProvider> */}
+        <ToastContainer theme="dark" />
+        <App />
+        {/* </AuthProvider> */}
       </MarkdownProvider>
     </ThemeContextProvider>
   </React.StrictMode>,
